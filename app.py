78be89bed6743056e3c011d1b6daf3e8e320e341
vitaliyapostolov83 <vitaliyapostolov83@gmail.com>
2025-03-02 01:00:31 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,22 @@ import streamlit as st
 import pandas as pd
 import requests
 from io import StringIO
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
 
-st.title("Модель звіту по меблевику")# Заголовок
-number = st.slider("Виберіть кількість рядків таблиці", 1, 1000)# Додавання слайдера
+# Заголовок
+st.title("Модель звіту по меблевику")
+
+# Додавання слайдера
+number = st.slider("Виберіть кількість рядків таблиці", 1, 1000)
 st.write(f"Ви вибрали кількість рядків: {number}")
-url = "https://raw.githubusercontent.com/vitaliyapostolov83/report-model/main/flats.csv" # URL до файлу на GitHub (візьми Raw файл)
-response = requests.get(url) # Завантажуємо файл
+
+# URL до файлу на GitHub (візьми Raw файл)
+url = "https://raw.githubusercontent.com/vitaliyapostolov83/report-model/main/flats.csv"
+
+# Завантажуємо файл
+response = requests.get(url)
 
 # Якщо файл був успішно завантажений
 if response.status_code == 200:
@@ -19,6 +29,18 @@ if response.status_code == 200:
     
     # Вивести таблицю з налаштуванням ширини колонок
     st.dataframe(data, width=700)  # Регулює ширину всієї таблиці (у пікселях)
+
+    # Створення графіка
+    fig, ax = plt.subplots(figsize=(10, 6))  # Розмір графіка
+    sns.scatterplot(x=data['column_name_1'], y=data['column_name_2'], ax=ax)  # Замість 'column_name_1' і 'column_name_2' поставте ваші колонки
+
+    # Додавання назв осей
+    ax.set_xlabel('Назва осі X')
+    ax.set_ylabel('Назва осі Y')
+    ax.set_title('Графік залежності')
+
+    # Виведення графіка
+    st.pyplot(fig)
     
 else:
     st.write("Не вдалося завантажити файл.")

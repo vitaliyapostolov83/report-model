a2f43c42e6bc33db3f6ea38e33eaa8ec4a9ba509
vitaliyapostolov83 <vitaliyapostolov83@gmail.com>
2025-03-02 00:49:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,28 +3,15 @@ import pandas as pd
 import requests
 from io import StringIO
 
-# Заголовок
-st.title("Модель звіту по меблевику")
-
-# Додавання слайдера
-number = st.slider("Виберіть кількість рядків таблиці", 1, 1000)
+st.title("Модель звіту по меблевику")# Заголовок
+number = st.slider("Виберіть кількість рядків таблиці", 1, 1000)# Додавання слайдера
 st.write(f"Ви вибрали кількість рядків: {number}")
+url = "https://raw.githubusercontent.com/vitaliyapostolov83/report-model/main/flats.csv" # URL до файлу на GitHub (візьми Raw файл)
+response = requests.get(url) # Завантажуємо файл
 
-# URL до файлу на GitHub (візьми Raw файл)
-url = "https://raw.githubusercontent.com/vitaliyapostolov83/report-model/main/flats.csv"  # Замінено на правильний шлях до твого файлу
-
-# Завантажуємо файл
-response = requests.get(url)
-
-# Якщо файл був успішно завантажений
-if response.status_code == 200:
-    # Завантажуємо CSV в Pandas DataFrame
-    data = pd.read_csv(StringIO(response.text))
-    
-    # Обмежуємо кількість рядків відповідно до вибору на слайдері
-    data = data.head(number)
-    
-    # Вивести обмежену таблицю на сторінці Streamlit
-    st.write(data)
+if response.status_code == 200:# Якщо файл був успішно завантажений
+    data = pd.read_csv(StringIO(response.text))# Завантажуємо CSV в Pandas DataFrame
+    data = data.head(number)# Обмежуємо кількість рядків відповідно до вибору на слайдері
+    st.write(data)# Вивести обмежену таблицю на сторінці Streamlit
 else:
     st.write("Не вдалося завантажити файл.")

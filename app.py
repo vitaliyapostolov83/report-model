147fa1e5d2ee04cfcecd1650ee892e04e386dcd3
vitaliyapostolov83 <vitaliyapostolov83@gmail.com>
2025-03-01 22:33:54 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+# Заголовок
+st.title("Модель звіту")
+
+# Додавання слайдера
+number = st.slider("Виберіть кількість рядків таблиці", 1, 1000)
+st.write(f"Ви вибрали кількість строк {number}")
+
+# Створення таблиці з Pandas
+data = pd.DataFrame({
+    'Число': np.arange(1, number+1),
+    'Квадрат': np.arange(1, number+1) ** 2
+})
+
+# Виведення таблиці
+st.write(data)
